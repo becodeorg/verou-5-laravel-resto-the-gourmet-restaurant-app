17b85ca3981a9bfaf84cce5b86f0086c888e1eb8
LucasDC13 <lucas_de_corte@hotmail.com>
2024-02-05 15:32:20 +0100

    Add menu styling

--- laravel/resources/views/guest/menu.blade.php
+++ laravel/resources/views/guest/menu.blade.php
@@ -6,103 +6,131 @@
 
 @section('content')
     <main>
-        <section>
-            <h3>Drinks</h3>
-            <menu>
+        <figure class="chef-warning">
+            <img src="{{ asset('assets/the-gourmet.webp') }}" alt="">
+            <figcaption>DON'T ORDER OFF MENU!!!</figcaption>
+        </figure>
+
+        <section class='menu-category'>
+            <h3>Alcoholic Beverages</h3>
+            <menu class='menu-list'>
+            @foreach ($products
+            ->where('category', 'Drinks')
+            ->where('has_alcohol', 1) as $item)
                 <li>
                     <div>
-                        <h4>Food item name</h4>
-                        <p>Food item description</p>
-                        <p>Food item price</p>
+                        <h4>{{ $item->name }}</h4>
+                        <p>{{ $item->price }}</p>
+                        <p>{{ $item->allergies }}</p>
+
                     </div>
                 </li>
-                <li>
-                    <div>
-                        <h4>Food item name</h4>
-                        <p>Food item description</p>
-                        <p>Food item price</p>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <h4>Food item name</h4>
-                        <p>Food item description</p>
-                        <p>Food item price</p>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <h4>Food item name</h4>
-                        <p>Food item description</p>
-                        <p>Food item price</p>
-                    </div>
-                </li>
+            @endforeach
             </menu>
         </section>
-        <section>
-            <h3>Wine</h3>
-            <menu>
+        <hr>
+
+        
+
+        <section class='menu-category'>
+            <h3>Non-alcoholic Beverages</h3>
+            <menu class='menu-list'>
+            @foreach ($products
+            ->where('category', 'Drinks')
+            ->where('has_alcohol', 0) as $item)
                 <li>
                     <div>
-                        <h4>Food item name</h4>
-                        <p>Food item description</p>
-                        <p>Food item price</p>
+                        <h4>{{ $item->name }}</h4>
+                        <p>{{ $item->price }}</p>
+                        <p>{{ $item->allergies }}</p>
                     </div>
                 </li>
-                <li>
-                    <div>
-                        <h4>Food item name</h4>
-                        <p>Food item description</p>
-                        <p>Food item price</p>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <h4>Food item name</h4>
-                        <p>Food item description</p>
-                        <p>Food item price</p>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <h4>Food item name</h4>
-                        <p>Food item description</p>
-                        <p>Food item price</p>
-                    </div>
-                </li>
+            @endforeach
             </menu>
         </section>
-        <section>
-            <h3>Cocktails</h3>
-            <menu>
+        <hr>
+
+        <section class='menu-category'>
+            <h3>Appetizers</h3>
+            <menu class='menu-list'>
+            @foreach ($products
+            ->where('category', 'Appetizers') as $item)
                 <li>
                     <div>
-                        <h4>Food item name</h4>
-                        <p>Food item description</p>
-                        <p>Food item price</p>
+                        <h4>{{ $item->name }}</h4>
+                        <p>{{ $item->price }}</p>
+                        <p>{{ $item->allergies }}</p>
                     </div>
                 </li>
+            @endforeach
+            </menu>
+        </section>
+        <hr>
+
+        <section class='menu-category'>
+            <h3>Soups</h3>
+            <menu class='menu-list'>
+            @foreach ($products
+            ->where('category', 'Soups') as $item)
                 <li>
                     <div>
-                        <h4>Food item name</h4>
-                        <p>Food item description</p>
-                        <p>Food item price</p>
+                        <h4>{{ $item->name }}</h4>
+                        <p>{{ $item->price }}</p>
+                        <p>{{ $item->allergies }}</p>
                     </div>
                 </li>
+            @endforeach
+            </menu>
+        </section>
+        <hr>
+
+        <section class='menu-category'>
+            <h3>Salads</h3>
+            <menu class='menu-list'>
+            @foreach ($products
+            ->where('category', 'Salads') as $item)
                 <li>
                     <div>
-                        <h4>Food item name</h4>
-                        <p>Food item description</p>
-                        <p>Food item price</p>
+                        <h4>{{ $item->name }}</h4>
+                        <p>{{ $item->price }}</p>
+                        <p>{{ $item->allergies }}</p>
                     </div>
                 </li>
+            @endforeach
+            </menu>
+        </section>
+        <hr>
+
+        <section class='menu-category'>
+            <h3>Main Courses</h3>
+            <menu class='menu-list'>
+            @foreach ($products
+            ->where('category', 'Main Courses') as $item)
                 <li>
                     <div>
-                        <h4>Food item name</h4>
-                        <p>Food item description</p>
-                        <p>Food item price</p>
+                        <h4>{{ $item->name }}</h4>
+                        <p>{{ $item->price }}</p>
+                        <p>{{ $item->allergies }}</p>
                     </div>
                 </li>
+            @endforeach
+            </menu>
+        </section>
+        <hr>
+
+        <section class='menu-category'>
+            <h3>Desserts</h3>
+            <menu class='menu-list'>
+            @foreach ($products
+            ->where('category', 'Desserts') as $item)
+                <li>
+                    <div>
+                        <h4>{{ $item->name }}</h4>
+                        <p>{{ $item->price }}</p>
+                        <p>{{ $item->allergies }}</p>
+                    </div>
+                </li>
+            @endforeach
             </menu>
         </section>
     </main>
--- laravel/resources/views/layout/guest/header.blade.php
+++ laravel/resources/views/layout/guest/header.blade.php
@@ -1,4 +1,8 @@
 <header class="header">
+    <figure class="header-fig">
+        <img class="header-img" src="{{ asset('assets/chef-hat.webp') }}" alt="A chef's hat out of the critically acclaimed 2011 video game, The Elder Srolls V: Skyrim.">
+    </figure>
+    <h1 class="header-h1">The Gourmet's Gourmet Gourmet</h1>
     <nav class="header-nav">
         <ul class="header-ul">
             <li><a href="{{ route('guest') }}">Home</a></li>

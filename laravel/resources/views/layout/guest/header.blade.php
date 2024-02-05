77a87db625c5dadad2c42d21dd8c9ea0206fd76b
LucasDC13 <lucas_de_corte@hotmail.com>
2024-02-05 11:36:50 +0100

    Test git push

--- laravel/resources/views/layout/guest/header.blade.php
+++ laravel/resources/views/layout/guest/header.blade.php
@@ -3,6 +3,7 @@
         <ul class="header-ul">
             <li><a href="{{ route('reserve') }}">Reserve</a></li>
             <li><a href="{{ route('menu') }}">Menu</a></li>
+            <li><a href="{{ route('about') }}">About</a></li>
         </ul>
     </nav>
 </header>

6d8458a8d40ff7bd4752b948cb36b3edda532516
LucasDC13 <lucas_de_corte@hotmail.com>
2024-02-05 12:46:04 +0100

    Style landing page

--- laravel/resources/views/layout/footer.blade.php
+++ laravel/resources/views/layout/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-    <p class="footer-text">Footer</p>
+    <p class="footer-text">The name 'the Gourmet', as well as any dishes mentioned on this site, are the intellectual property of Balagog gro-Nolob, the noble Breton Orc chef. &copy; {{ Date ('Y') }} (This website is for educational purposes only)</p>
 </footer>
--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'The Gourmet')</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
     <header class="header">
@@ -14,12 +14,16 @@
         <h1 class="header-h1">The Gourmet's Gourmet Gourmet</h1>
     </header>
     <main class='welcomeForms'>
-        <form class='welcomeForm' action="{{ route('guest') }}" method="POST">
+        <form class='welcomeForm-guest' action="{{ route('guest') }}" method="POST">
             @csrf
             <input class='button' type="submit" value="Continue as Guest"></input>
         </form>
-        <form class='welcomeForm' action="{{ route('loginEmployee') }}" method="POST">
+        <form class='welcomeForm-employee' action="{{ route('loginEmployee') }}" method="POST">
             @csrf
+            <label for='employeeName'>Username:</label>
+            <input name='employeeName' id='employeeName' type='text'></input> 
+            <label for='employeePass'>Password:</label>
+            <input name='employeePass' id='employeePass' type='text'></input> 
             <input class='button' type="submit" value="Continue as Employee"></input>
         </form>
     </main>

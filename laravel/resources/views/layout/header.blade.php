04de128fdfae652887c4e3d113a0360b8c78e502
LucasDC13 <lucas_de_corte@hotmail.com>
2024-02-05 11:30:20 +0100

    Trying to fix git

--- laravel/resources/views/layout/header.blade.php
+++ laravel/resources/views/layout/header.blade.php
@@ -5,7 +5,9 @@
     <h1>The Gourmet's Gourmet Gourmet</h1>
     <nav class="header-nav">
         <ul class="header-ul">
-            <li>Header</li>
+            <li><a href="">About</a></li>
+            <li><a href="">Menu</a></li>
+            <li><a href="">Make a reservation</a></li>
         </ul>
     </nav>
 </header>

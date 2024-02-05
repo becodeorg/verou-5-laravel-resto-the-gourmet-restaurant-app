e9d854f9ebb3ea9bf41ff8b20b1e6931cf074ead
LucasDC13 <lucas_de_corte@hotmail.com>
2024-02-05 14:42:14 +0100

    Add menu placeholder

--- laravel/resources/views/guest/menu.blade.php
+++ laravel/resources/views/guest/menu.blade.php
@@ -6,6 +6,104 @@
 
 @section('content')
     <main>
-        <p>Menu</p>
+        <section>
+            <h3>Drinks</h3>
+            <menu>
+                <li>
+                    <div>
+                        <h4>Food item name</h4>
+                        <p>Food item description</p>
+                        <p>Food item price</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h4>Food item name</h4>
+                        <p>Food item description</p>
+                        <p>Food item price</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h4>Food item name</h4>
+                        <p>Food item description</p>
+                        <p>Food item price</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h4>Food item name</h4>
+                        <p>Food item description</p>
+                        <p>Food item price</p>
+                    </div>
+                </li>
+            </menu>
+        </section>
+        <section>
+            <h3>Wine</h3>
+            <menu>
+                <li>
+                    <div>
+                        <h4>Food item name</h4>
+                        <p>Food item description</p>
+                        <p>Food item price</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h4>Food item name</h4>
+                        <p>Food item description</p>
+                        <p>Food item price</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h4>Food item name</h4>
+                        <p>Food item description</p>
+                        <p>Food item price</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h4>Food item name</h4>
+                        <p>Food item description</p>
+                        <p>Food item price</p>
+                    </div>
+                </li>
+            </menu>
+        </section>
+        <section>
+            <h3>Cocktails</h3>
+            <menu>
+                <li>
+                    <div>
+                        <h4>Food item name</h4>
+                        <p>Food item description</p>
+                        <p>Food item price</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h4>Food item name</h4>
+                        <p>Food item description</p>
+                        <p>Food item price</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h4>Food item name</h4>
+                        <p>Food item description</p>
+                        <p>Food item price</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <h4>Food item name</h4>
+                        <p>Food item description</p>
+                        <p>Food item price</p>
+                    </div>
+                </li>
+            </menu>
+        </section>
     </main>
 @endsection

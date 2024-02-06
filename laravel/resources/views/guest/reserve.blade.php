73ce5d61e3fe92641a91a3b89004b6cd10e63e5a
LucasDC13 <lucas_de_corte@hotmail.com>
2024-02-06 12:06:54 +0100

    Style reservation form

--- laravel/resources/views/guest/reserve.blade.php
+++ laravel/resources/views/guest/reserve.blade.php
@@ -5,13 +5,13 @@
 @endsection
 
 @section('content')
-    <main>
-        <form action="{{ route('reservations.store') }}" method="POST">
+    <main class='reservationWrapper'>
+        <form class='reservationForm' action="{{ route('reservations.store') }}" method="POST">
             @csrf
             <label for="reservationName">Name:</label><br>
-            <input type="text" name="reservationName" id="reservationName"><br>
+            <input type="text" name="reservationName" id="reservationName" class="reservationHalfWidth"><br>
             <label for="reservationEmail">Email:</label><br>
-            <input type="email" name="reservationEmail" id="reservationEmail"><br>
+            <input type="email" name="reservationEmail" id="reservationEmail" class="reservationHalfWidth"><br>
             <label for="reservationDate">Date:</label><br>
             <input type="date" name="reservationDate" id="reservationDate"><br>
             <label for="reservationTime">Hour:</label><br>
@@ -33,8 +33,8 @@
                 @endforeach
             </select><br>
             <label for="reservationNotes">Notes:</label><br>
-            <textarea name="reservationNotes" id="reservationNotes" cols="30" rows="10" placeholder="Add some notes if you have some.."></textarea><br>
-            <input type="submit" value="Make Reservation">
+            <textarea class='reservationFullWidth' name="reservationNotes" id="reservationNotes" cols="30" rows="10" placeholder="Add some notes if you have some.."></textarea><br>
+            <input class="button" type="submit" value="Make Reservation">
         </form>
     </main>
 @endsection

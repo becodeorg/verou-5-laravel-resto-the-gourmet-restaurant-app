4a7ec9a47ac7b62fda946ed7fe9982b73929c670
Jonasi-Deetens <jonasideetens@hotmail.com>
2024-02-09 11:48:01 +0100

    Change guest header to include about page and home button

--- laravel/resources/views/layout/guest/header.blade.php
+++ laravel/resources/views/layout/guest/header.blade.php
@@ -5,7 +5,8 @@
     <h1 class="header-h1">The Gourmet's Gourmet Gourmet</h1>
     <nav class="header-nav">
         <ul class="header-ul">
-            <li><a class='button' href="{{ route('guest') }}">Home</a></li>
+            <li><a class='button' href="{{ route('home') }}">Home</a></li>
+            <li><a class='button' href="{{ route('guest') }}">About</a></li>
             <li><a class='button' href="{{ route('menu') }}">Menu</a></li>
             <li><a class='button reserve-button' href="{{ route('reservations.create') }}">Reserve Table</a></li>
         </ul>

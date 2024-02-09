48bba5e7bfeeed7e17527d71f9109a80c296ac2f
LucasDC13 <lucas_de_corte@hotmail.com>
2024-02-09 14:21:06 +0100

    Clean CSS

--- laravel/resources/views/guest/index.blade.php
+++ laravel/resources/views/guest/index.blade.php
@@ -8,7 +8,7 @@
     <main class="home-wrapper">
         <article id="restaurant-info">
             <h2 class="guest-title">About Us</h2>
-            <p>Welcome to <b><i>The Gourmet's Gourmet Gourmet</i></b> \- where culinary excellence meets unparalleled creativity! Step into a world where every dish tells a story, and every flavor ignites your senses. Inspired by the legendary chef from Skyrim and his renowned tome "Uncommon Taste," our restaurant promises a gastronomic adventure like no other.</p>
+            <p>Welcome to <b><i>The Gourmet's Gourmet Gourmet</i></b> - where culinary excellence meets unparalleled creativity! Step into a world where every dish tells a story, and every flavor ignites your senses. Inspired by the legendary chef from Skyrim and his renowned tome "Uncommon Taste," our restaurant promises a gastronomic adventure like no other.</p>
             <br>
             <p>At <b><i>The Gourmet's Gourmet Gourmet</i></b>, we invite you to indulge in a symphony of flavors crafted with passion and precision, drawing inspiration from the pages of "Uncommon Taste." From delectable appetizers to exquisite main courses and divine desserts, each creation is a masterpiece in itself, resonating with the spirit of Skyrim's culinary heritage. Whether you're a seasoned epicurean or a curious food enthusiast, prepare to embark on a journey of taste and discovery. Join us and experience the epitome of fine dining – where every bite is a celebration of culinary mastery. Welcome to a realm where food transcends mere sustenance and becomes an art form. Welcome to <b><i>The Gourmet's Gourmet Gourmet</i></b> – where every meal is an unforgettable experience!</p>
         </article>
